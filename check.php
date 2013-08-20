6d554f5094f7f844e3eb385507ee1420e23ae535
Rohit Jha <rj@rohitjha.com>
2013-08-20 23:44:55 +0800

    appid/key secret added

--- check.php
+++ check.php
@@ -2,6 +2,11 @@
 require('Pusher.php');
 echo '<div id="message">Waiting for you to confirm login from remote device.....</div>';
 $user = $_GET['user'];
+$app_id = '44376';
+$app_key = '90589c5b71118c012d39';
+$app_secret = 'd5ec30897a541bd86cb5';
+
+$pusher = new Pusher( $app_key, $app_secret, $app_id ,false, 'https://api.pusherapp.com', 443);
 $pusher->trigger('Konsensus', 'login-event', array('user' => $user) );
 
 echo '<script src="http://js.pusher.com/2.0/pusher.min.js"></script>
